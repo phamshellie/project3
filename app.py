24de501a55a855b9e554fec862f001e33a3efe49
phamshellie <105142377+phamshellie@users.noreply.github.com>
2023-12-10 18:27:59 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from auth import auth_blueprint
+from database import init_db
+
+app = Flask(__name__)
+app.register_blueprint(auth_blueprint)
+init_db()  # Initialize database tables
+
+if __name__ == '__main__':
+    app.run(debug=True)
